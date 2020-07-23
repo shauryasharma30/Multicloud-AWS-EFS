4178419f99208b088b11bccd8fa91e3728f10dba
Shaurya <60067200+shauryasharma30@users.noreply.github.com>
2020-07-23 15:46:49 +0530

    ...

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
   
 <center>HYBRID-CLOUD-COMPUTING / TASK-2 / SHAURYA</center>
 <br />
-<img src = 'd1ghvwlrox2e4x.cloudfront.net' width ='100%' height ='100%' />
+<img src = 'https://d1ghvwlrox2e4x.cloudfront.net/t2.png' width ='100%' height ='100%' />
