68e0180ae4e452246f28889e5b8f7d0fb733468c
Shaurya <60067200+shauryasharma30@users.noreply.github.com>
2020-07-24 19:21:40 +0530

    Initial commit

---
+++ S3andCloudfront.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  region     = "ap-south-1"
+  profile    = "task2"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "mybucket787"
+  acl    = "private"
+
+  tags = {
+    Name = "Mybucket-task2"
+  }
+}
+
+
+resource "aws_s3_bucket_object" "image" {
+depends_on = [
+aws_s3_bucket.bucket,
+]
+bucket = "mybucket787"
+	key = "t2.png"
+        source = "C:/Users/shaurya/Desktop/TASK2/photo/t2.png"
+	etag = filemd5("C:/Users/shaurya/Desktop/TASK2/photo/t2.png")
+	
+	acl = "public-read"
+
+}
+
+resource "aws_s3_bucket_public_access_block" "type" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+resource "aws_cloudfront_distribution" "webcloud" {
+  origin {
+    domain_name = "${aws_s3_bucket.bucket.bucket_regional_domain_name}"
+    origin_id   = "${local.s3_origin_id}"
+custom_origin_config {
+    http_port = 80
+    https_port = 80
+    origin_protocol_policy = "match-viewer"
+    origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"] 
+    }
+  }
+enabled = true
+default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${local.s3_origin_id}"
+forwarded_values {
+    query_string = false
+cookies {
+      forward = "none"
+      }
+    }
+viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+resource "null_resource" "webip"  {
+ provisioner "local-exec" {
+     command = "echo  ${aws_cloudfront_distribution.webcloud.domain_name} > cloudfront.txt"
+   }
+}
---
+++ instance.tf
@@ -0,0 +1,138 @@
+provider "aws" {
+  region     = "ap-south-1"
+  profile    = "task2"
+}
+
+resource "aws_key_pair" "key" {
+key_name = "mykeytask2"
+public_key="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAjl4x/ZdCZ1ykOdVNSzR9LcOo4hS1YZlDEOLu2LfX3SvlpxmeBh9HKn2VvEoHHLPoKGHNShtLf74M2J4XF1MKiCSxi5d+UZtv0125F3XqGVymVmKJFlOPvVfZ4tpu5hVWadVLl4IzQbbsnfK5Dy3vYZY/W+j/qv4Ps3QBm3HiRkURFFqxFLOexPMMNq3nk0DLArFv50CA2PpIFYZ+OnW+Uklf+6cpbQyWn3Qk0oZMUk2UxInfiyORLVkl3mU3BUDK9Hq8WUCjf/GB3Q0iGZo1rIIjL8tUdq3nK/9UWyShuWqMYyyW7uZ3ZVOeiBettc+keggulF/eI+apH+JSJfBvsw== rsa-key-20200611"
+}
+
+resource "aws_security_group" "task2sg" {
+  name        = "add_rules"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-ebe9f483"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp" 
+    cidr_blocks=["0.0.0.0/0"]
+ }
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp" 
+    cidr_blocks=["0.0.0.0/0"]
+ }
+  ingress {
+    description = "NFS"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp" 
+    cidr_blocks=["0.0.0.0/0"]
+  }
+   egress {
+    description = "HTTP from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp" 
+    cidr_blocks=["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Task-2/SG"
+  }
+}
+
+data "aws_vpc" "default" {
+  default="true"
+}
+data "aws_subnet" "subnet" {
+  vpc_id = data.aws_vpc.default.id
+  availability_zone="ap-south-1a"
+}
+
+resource "aws_efs_file_system" "task2efs" {
+  creation_token = "t2_efs"
+
+  tags = {
+    Name = "Persistent/EFS"
+  }
+}
+resource "aws_efs_mount_target" "mount" {
+  depends_on = [aws_efs_file_system.task2efs]
+  file_system_id = aws_efs_file_system.task2efs.id
+  subnet_id      = data.aws_subnet.subnet.id
+  security_groups = [ aws_security_group.task2sg.id ]
+}
+
+
+resource "aws_instance" "inst" {
+  ami           = "ami-052c08d70def0ac62"
+  instance_type = "t2.micro"
+  key_name = "mykeytask2"
+  vpc_security_group_ids=[aws_security_group.task2sg.id]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("C:/Users/shaurya/Downloads/task-key.pem")
+    host     = aws_instance.inst.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd php git -y",
+      "sudo setenforce 0",
+      "sudo yum install amazon-efs-utils -y",
+      "sudo yum install nfs-utils -y",
+      "sudo systemctl restart httpd",
+      "sudo systemctl enable httpd",
+      ]
+  }
+
+  tags = {
+    Name = "TASK2-OS"
+  }
+
+}
+
+resource "null_resource" "local1"  {
+	provisioner "local-exec" {
+	    command = "echo  ${aws_instance.inst.public_ip} > publicip.txt"
+  	}
+}
+
+
+resource "null_resource" "remote2"  {
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("C:/Users/shaurya/Downloads/task-key.pem")
+    host     = aws_instance.inst.public_ip
+  }
+
+provisioner "remote-exec" {
+    inline = [
+       "sudo mount -t efs ${aws_efs_file_system.task2efs.id}:/ /var/www/html",
+       "echo '${aws_efs_file_system.task2efs.id}:/ /var/www/html efs _netdev 0 0' | sudo tee -a sudo tee -a /etc/fstab",
+       "sudo rm -rf /var/www/html/*",
+       "sudo git clone https://github.com/shauryasharma30/Multicloud-AWS-EFS.git /var/www/html/",
+    ]
+  }
+}
+
+
+
+
+
+
